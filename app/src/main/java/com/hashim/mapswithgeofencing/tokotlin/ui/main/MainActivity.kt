5ccdc561ce1c7c7bf025763511193849c3f7baa7
hashim <hashimtahir@live.com>
2021-03-17 22:45:16 +0500

    NavHost and main layout changes.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/mapswithgeofencing/tokotlin/ui/main/MainActivity.kt
+++ app/src/main/java/com/hashim/mapswithgeofencing/tokotlin/ui/main/MainActivity.kt
@@ -3,15 +3,36 @@ package com.hashim.mapswithgeofencing.tokotlin.ui.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.hashim.mapswithgeofencing.R
+import com.hashim.mapswithgeofencing.databinding.ActivityMain2Binding
 
 class MainActivity : AppCompatActivity() {
-    val hMainViewModel: MainViewModel by viewModels()
+    private val hMainViewModel: MainViewModel by viewModels()
+    private lateinit var hActivityMainBinding: ActivityMain2Binding
+
+    private lateinit var hNavHostFragments: NavHostFragment
+
+    private lateinit var hNavController: NavController
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        hActivityMainBinding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(hActivityMainBinding.root)
 
+        hInitNavView()
+    }
+
+
+    private fun hInitNavView() {
+        hNavHostFragments = supportFragmentManager
+                .findFragmentById(R.id.hMainFragmentContainerV) as NavHostFragment
+        hNavController = hNavHostFragments.navController
+
+        hNavController.setGraph(R.navigation.main_nav)
+        NavigationUI.setupWithNavController(hActivityMainBinding.hBottomNav, hNavController)
     }
 }
